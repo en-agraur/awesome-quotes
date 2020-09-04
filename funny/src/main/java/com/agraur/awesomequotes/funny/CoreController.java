05c7b5d6e25083f1ea9605b16710ffa2f7e34155
Gabriel Blaj <gabriel.blaj@endava.com>
2020-09-04 15:19:37 +0300

    updated version

--- funny/src/main/java/com/agraur/awesomequotes/funny/CoreController.java
+++ funny/src/main/java/com/agraur/awesomequotes/funny/CoreController.java
@@ -1,12 +1,14 @@
 package com.agraur.awesomequotes.funny;
 
+import com.agraur.awesomequotes.core.CoreApi;
 import com.agraur.awesomequotes.core.Quote;
 
 /**
  * Created by Alex.Graur@endava.com at 9/3/2020
  */
-public class CoreController {
+public class CoreController implements CoreApi {
 
+    @Override
     public Quote getRandomQuote() {
         return new Quote("People say nothing is impossible. But I do nothing every day.", "A. A. Milne");
     }
